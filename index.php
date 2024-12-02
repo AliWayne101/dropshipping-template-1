508e1c3c222f5c39ae3bc1a8a79551b42074c6c7
AliMalik101 <alimalikwayne@gmail.com>
2024-12-02 18:08:54 +0500

    first upl

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+include './vars/_conf.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php constant('WEBNAME'); ?></title>
+    <link rel="stylesheet" href="css/vars.css">
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/sections/hero.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <nav>
+        <div class="nav">
+            <div>
+                <a href="/">LOGO</a>
+            </div>
+            <div class="nav-menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="body">
+
+        <div class="hero">
+            this is it
+        </div>
+        
+        <div class="content">
+            this is awesome
+        </div>
+        
+    </div>
+
+    <div class="footer">
+        This is footer
+    </div>
+</body>
+
+</html>
